9e894dec15bba3743bf072837ac70a9323a5f187
iseungsoo07 <iseungsoo07@gmail.com>
2024-11-19 10:16:32 +0900

    planthostel init

---
+++ src/test/java/com/planthostelserver/PlanthostelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planthostelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanthostelServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
